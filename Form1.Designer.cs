7a1a80797522fa9edc0d41865b61e89a24500adc
Piotr <75029082+Aomine2K@users.noreply.github.com>
2021-12-21 14:47:31 +0100

    Upload_1

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Generator_CD_KEY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDlugosc = new System.Windows.Forms.TextBox();
+            this.txtKlucz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generuj klucz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ilość znaków";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wygenerowany klucz";
+            // 
+            // txtDlugosc
+            // 
+            this.txtDlugosc.Location = new System.Drawing.Point(271, 138);
+            this.txtDlugosc.Name = "txtDlugosc";
+            this.txtDlugosc.Size = new System.Drawing.Size(50, 20);
+            this.txtDlugosc.TabIndex = 3;
+            // 
+            // txtKlucz
+            // 
+            this.txtKlucz.Location = new System.Drawing.Point(271, 187);
+            this.txtKlucz.Name = "txtKlucz";
+            this.txtKlucz.Size = new System.Drawing.Size(259, 20);
+            this.txtKlucz.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKlucz);
+            this.Controls.Add(this.txtDlugosc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDlugosc;
+        private System.Windows.Forms.TextBox txtKlucz;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generator_CD_KEY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string klucz = "";
+            int dlugoscKlucza = int.Parse(txtDlugosc.Text);
+            int liczbaZnakow = 0;
+                                 
+            Random liczbaLosowa = new Random();
+            while (liczbaZnakow < dlugoscKlucza)
+            {
+                int cyfra = liczbaLosowa.Next(0, 9);
+                klucz += cyfra.ToString();
+                liczbaZnakow++;
+                if (liczbaZnakow % 5 == 0 && liczbaZnakow < dlugoscKlucza)
+                
+                {
+                    klucz += "-";
+                }
+            }
+            txtKlucz.Text = klucz;
+        }
+    }
+}
